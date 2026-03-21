a3bd1ffe681e0a177d5cf2268b86991e999024ab
louplapene <louplapene@gmail.com>
2026-03-21 11:40:49 +0100

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,24 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(amount: number, currency = "EUR") {
+  return new Intl.NumberFormat("fr-FR", {
+    style: "currency",
+    currency,
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
+
+export function formatDate(date: string | Date | null | undefined) {
+  if (!date) return "â";
+  return new Intl.DateTimeFormat("fr-FR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  }).format(new Date(date));
+}
+
